cf2ffbc8ef57011f2cf9127d10c0aff7658d683d
veeraTommila <78955844+veeraTommila@users.noreply.github.com>
2021-10-22 14:49:48 +0300

    Added const int GuesTimes, while loop, int userInput, int minValue, int maxValue, var randomNumber, var lottoNumber, and two if else-functions.

--- C_sharp_game.cs
+++ C_sharp_game.cs
@@ -6,9 +6,33 @@ namespace C__game
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Guess the number");
-            string number = Console.ReadLine();
-            public static System.Security.Cryptography.RandomNumberGenerator Create ();
+            const int GuesTimes = 3;
+                int guessIndexes = 0;
+                while (true)
+                {
+                    Console.WriteLine("Guess the number!");
+
+                    int userInput = int.Parse(Console.ReadLine());
+
+                    int minValue = 1;
+                    int maxValue = 20;
+                    var randomNumber = new Random();
+                    var lottoNumber = randomNumber.Next(minValue, maxValue);
+                    if (lottoNumber == userInput)
+                    {
+                        Console.WriteLine("You Win!");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Wrong. You lose.");
+                        guessIndexes++;
+                    if (GuesTimes == guessIndexes)
+                    {
+                        Console.WriteLine("You will be executed!");
+                        break;}
+                    }
+                }
 
 
         }
