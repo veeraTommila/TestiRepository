7e52173c096cf71bbd20825d4557d63fc36cf5f1
veeraTommila <78955844+veeraTommila@users.noreply.github.com>
2021-11-01 13:21:25 +0200

    Created a database file named Mydatabase.db.

--- Database_things/sqlite_demo_creation.py
+++ Database_things/sqlite_demo_creation.py
@@ -1,2 +1,20 @@
 import sqlite3
 from sqlite3 import Error
+
+def create_connection(db_file):
+    """ create a database connection to SQLite database """
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)        
+        print(sqlite3.version)
+    except Error as e:
+        print(e)
+    finally:
+        if conn:
+            conn.close()
+
+
+if __name__ == '__main__':
+    create_connection(r"D:\\TestiRepository\\Database_things\\Mydatabase.db") #A connection to an SQLite database.
+
+
