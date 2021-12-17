5f6802359088d9cf02ef4605539dd4695243aa23
veeraTommila <78955844+veeraTommila@users.noreply.github.com>
2021-12-17 14:04:40 +0200

    Added a new folder HeiMaailma in Java-folder.

--- Java/HeiMaailma/src/Hei.java
+++ Java/HeiMaailma/src/Hei.java
@@ -3,7 +3,7 @@ public class Hei {
 
 	public static void main(String[] args) {
 		System.out.println("Hei, maailma!");
-		System.out.println("Hei kaikille!");
+		System.out.println("Hei kaikille!"); 
 		
 		int luku = 0;
 		
