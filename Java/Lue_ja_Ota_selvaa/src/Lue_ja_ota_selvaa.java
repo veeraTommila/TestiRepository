2cb8e941e82c0f9ebdb9a2158b7c0597fbd405d1
veeraTommila <78955844+veeraTommila@users.noreply.github.com>
2021-12-17 14:15:36 +0200

    Created a file called Lue_ja_Ota_selvaa and added a java file there called Lue_ja_ota_selvaa.java.

---
+++ Java/Lue_ja_Ota_selvaa/src/Lue_ja_ota_selvaa.java
@@ -0,0 +1,36 @@
+import java.io.File; 
+import java.io.FileNotFoundException; 
+import java.util.Scanner;
+
+public class Lue_ja_ota_selvaa {
+
+	public static void main(String[] args) {
+		try {
+			File tiedostoOlio1 = new File("pisteet.txt");
+			Scanner lukijaOlio = new Scanner(tiedostoOlio1);
+			while (lukijaOlio.hasNextLine()) {
+				String data = lukijaOlio.nextLine();
+				System.out.println(data);
+			}
+			lukijaOlio.close();
+		}catch(FileNotFoundException e) {
+			System.out.println("Tapahtui virhe.");
+			e.printStackTrace();
+		}
+		System.out.println("");
+		try {
+			File tiedostoOlio2 = new File("polygoni.txt");
+			Scanner lukijaOlio = new Scanner(tiedostoOlio2);
+			while (lukijaOlio.hasNextLine()) {
+				String data = lukijaOlio.nextLine();
+				System.out.println(data);
+			}
+			lukijaOlio.close();
+		}catch(FileNotFoundException e) {
+			System.out.println("Tapahtui virhe.");
+			e.printStackTrace();
+		}
+
+	}
+
+}
