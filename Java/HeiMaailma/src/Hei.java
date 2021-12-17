6912584bd7f070ef124b8b7c497f2dca05739164
veeraTommila <78955844+veeraTommila@users.noreply.github.com>
2021-12-17 14:03:29 +0200

    Created a new folder called Java.

---
+++ Java/HeiMaailma/src/Hei.java
@@ -0,0 +1,17 @@
+
+public class Hei {
+
+	public static void main(String[] args) {
+		System.out.println("Hei, maailma!");
+		System.out.println("Hei kaikille!");
+		
+		int luku = 0;
+		
+		for(int i = 0; i < 10; i++) {
+			luku++;
+			System.out.println("Hei, asukas kerroksessa "+luku+"!");
+		}
+				
+	}
+
+}
