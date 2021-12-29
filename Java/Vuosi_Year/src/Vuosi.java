98df9214da7e6f6ad83dc4d97f788afb23a78bca
veeraTommila <78955844+veeraTommila@users.noreply.github.com>
2021-12-29 12:50:53 +0200

    What is the current year in Finnish?

---
+++ Java/Vuosi_Year/src/Vuosi.java
@@ -0,0 +1,10 @@
+import java.time.Clock;
+import java.time.Year;
+
+public class Vuosi { 
+    public static void main(String[] args) {
+        Clock clock = Clock.systemDefaultZone();
+        Year vuosi = Year.now(clock);
+        System.out.println("Nyt on vuosi "+vuosi);
+    }
+}
