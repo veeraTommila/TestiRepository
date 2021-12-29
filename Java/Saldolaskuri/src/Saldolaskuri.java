eac651bad3e82693177c5a5018b5464d061b8c91
veeraTommila <78955844+veeraTommila@users.noreply.github.com>
2021-12-29 13:23:43 +0200

    To count the phone bill.

---
+++ Java/Saldolaskuri/src/Saldolaskuri.java
@@ -0,0 +1,17 @@
+public class Saldolaskuri {
+    public static void main(String[] args) {        
+               
+        double minuutit = 428.5; //Puhutut minuutit.
+        int tekstiviestit = 36; //Lähetetyt tekstiviestit
+
+        double minuuttihinta = 0.069;   //Euroa per minuutti.
+        double tekstiviestihinta = 0.069;   //Euroa per tekstiviesti. 
+
+        double laskettuMinuuttihinta = minuutit * minuuttihinta;    //428.5 * 0.069.
+        double laskettuTekstiviestihinta = tekstiviestit * tekstiviestihinta;   //36 * 0.069.
+
+        double yhteishinta = laskettuMinuuttihinta + laskettuTekstiviestihinta;
+        
+        System.out.println("Puhelinlaskun hinta: "+yhteishinta+" euroa.");
+    }    
+}
