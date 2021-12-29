5e193a3efbf975eda51a45d563ce1ec270c47ef5
veeraTommila <78955844+veeraTommila@users.noreply.github.com>
2021-12-29 15:00:20 +0200

    To count the kilometer allowance.

--- Java/KilometrikorvausLaskuri/src/KilometriKorvausLaskuri.java
+++ Java/KilometrikorvausLaskuri/src/KilometriKorvausLaskuri.java
@@ -1,7 +1,7 @@
 public class KilometriKorvausLaskuri {
     public static void main(String[] args) {        
         int kilometri = 275;
-        double korvausmaara = 0.44; 
+        double korvausmaara = 0.44;  
         double kilometrikorvaus = kilometri * korvausmaara;        
         System.out.println("0.44 euron kilometrikorvaus määrälle " + kilometri + " kilometriä on: " + kilometrikorvaus + " euroa.");        
     }    
