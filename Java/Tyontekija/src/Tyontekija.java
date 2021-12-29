b3b0a52949ecd1eb4e3ce4cd39cbaf6e91654277
veeraTommila <78955844+veeraTommila@users.noreply.github.com>
2021-12-29 12:40:39 +0200

    Created a folder 'Tyontekija' and added Java code there.

---
+++ Java/Tyontekija/src/Tyontekija.java
@@ -0,0 +1,27 @@
+public class Tyontekija {
+    public static void main(String[] args) {
+		String nimi = "Kalle Sörsselssön";
+		double palkka = 3200.5;
+		int ikä = 30;
+		String osoite = "Kukkulakuja 3, 00010 Kukkulakaupunki";
+		boolean lomalla = false;
+		
+		LocalDateTime syntymäAika = LocalDateTime.now();
+		DateTimeFormatter syntymäAjanMuoto = DateTimeFormatter.ofPattern("31-11-1991 01:00:22");
+		String muotoiltuPäivämäärä = syntymäAika.format(syntymäAjanMuoto);		
+		
+		System.out.println("Nimi: " + nimi);
+		System.out.println("palkka: " + palkka);
+		System.out.println("ikä: " + ikä);
+		System.out.println("Syntymäaika: " + muotoiltuPäivämäärä);
+		System.out.println("Osoite: " + osoite);
+		System.out.print("Onko työntekijä lomalla?");		
+		
+		if(lomalla == true) {
+			System.out.println(" "+ nimi + " on lomalla.");
+		}else {
+			System.out.println(" "+ nimi + " on töissä.");
+		}                                              		
+	}
+
+}
