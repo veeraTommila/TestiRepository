b9cb28d422fd1e065439e16e7ee8c55d344540e7
veeraTommila <78955844+veeraTommila@users.noreply.github.com>
2021-12-29 13:17:22 +0200

    A calculator for kilometer allowance.

---
+++ Java/KilometrikorvausLaskuri/src/KilometriKorvausLaskuri.java
@@ -0,0 +1,8 @@
+public class KilometriKorvausLaskuri {
+    public static void main(String[] args) {        
+        int kilometri = 275;
+        double korvausmaara = 0.44; 
+        double kilometrikorvaus = kilometri * korvausmaara;        
+        System.out.println("0.44 euron kilometrikorvaus määrälle " + kilometri + " kilometriä on: " + kilometrikorvaus + " euroa.");        
+    }    
+}
