5fac355ce46c6c487fca9775f3aaae96057d8b04
veeraTommila <78955844+veeraTommila@users.noreply.github.com>
2021-12-30 13:31:43 +0200

    Created a folder called Puhelu.

---
+++ Java/Puhelu/Puhelu.java
@@ -0,0 +1,23 @@
+/**
+ * Puhelu
+ */
+public class Puhelu {
+
+    double kesto;
+
+/**
+* Parametrillinen muodostin, jolla saadaan asetettua 
+* luotavan olion attribuutille haluttu alkuarvo.
+* */
+    public Puhelu(double kesto){
+        setKesto(kesto);
+    }
+
+    public getKesto(){
+        return kesto;   //Palautetaan haettavan attribuutin arvo.
+    }
+
+    public static void main(String[] args) {
+       
+    }
+}
